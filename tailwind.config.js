50cc326ac5afdd856712b2139b86b1c0db58d062
soekhant001 <khant253003@gmail.com>
2023-10-31 20:24:43 +0630

    already

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}","index.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        "lg":"1366px",
+      }
+    },
   },
   plugins: [],
 }
